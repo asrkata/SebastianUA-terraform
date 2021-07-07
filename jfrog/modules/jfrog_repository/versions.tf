0db37f20ec6c76e86470bc773100546850929123
Vitaliy Natarov <captain@MacBook.local>
2021-07-08 01:16:23 +0300

    Tmp

--- jfrog/modules/jfrog_repository/versions.tf
+++ jfrog/modules/jfrog_repository/versions.tf
@@ -1,9 +1,9 @@
-terraform {
-  required_providers {
-    artifactory = {
-      source  = "jfrog/artifactory"
-      version = "~> 2.2.11"
-    }
-  }
-  required_version = "~> 0.13"
-}
+// terraform {
+//   required_providers {
+//     artifactory = {
+//       source  = "jfrog/artifactory"
+//       version = "~> 2.2.11"
+//     }
+//   }
+//   required_version = "~> 0.13"
+// }
